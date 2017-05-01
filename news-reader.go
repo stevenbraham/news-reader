f1e832210357f54aef66e478e7fd6b4137702973
Steven Braham <steven@braham.biz>
2017-05-01 22:25:23 +0200

    read from websites.yaml and removed old websites package

--- news-reader.go
+++ news-reader.go
@@ -5,9 +5,10 @@ import (
 	"github.com/mmcdole/gofeed"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
-	"news-reader/websites"
 	"os"
 )
+
+var websites = []Website{}
 
 func main() {
 	loadWebsitesFromFile()
@@ -32,13 +33,16 @@ func loadWebsitesFromFile() {
 		ioutil.WriteFile("websites.yaml", encodedDefaultWebsites, 0777)
 
 	}
+	//import file and read websites
+	fileHandler, _ := ioutil.ReadFile("websites.yaml")
+	yaml.Unmarshal([]byte(fileHandler), &websites)
 }
 
 //Reads website from command line or displays an command line input tool
-func getSelectedWebsite() websites.Website {
+func getSelectedWebsite() Website {
 	if len(os.Args) == 2 {
 		websiteName := os.Args[1]
-		for _, website := range websites.Websites {
+		for _, website := range websites {
 			if website.Name == websiteName {
 				return website
 			}
@@ -48,11 +52,11 @@ func getSelectedWebsite() websites.Website {
 	}
 	//render input screen
 	fmt.Println("Choose a news source:")
-	for id, website := range websites.Websites {
+	for id, website := range websites {
 		fmt.Println(id, website.Name)
 	}
 	fmt.Print("\nid:")
 	index := 0
 	fmt.Scan(&index)
-	return websites.Websites[index] //index out of range will fire if the user selects an invalid id
+	return websites[index] //index out of range will fire if the user selects an invalid id
 }
